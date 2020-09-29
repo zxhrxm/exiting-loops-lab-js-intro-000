722a24ba92ea79cbdb9d179ad2e97abb00359592
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-29 14:20:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,10 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-
+  for (var i=0; i<array.length; i++) {
+    if(findFn(array[i])) {
+      return array[i]
+    }
+  }
+  return null
 }
