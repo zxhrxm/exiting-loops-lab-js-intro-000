08ec60cd212db67a722cbd3d9f5ed1095f301162
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-29 14:17:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i=0; i<array.length; i++) {
+    if (array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i=0; i<array.length; i++) {
+    if(array[i] === skipValue) {
+      continue
+    } else {
+      array[i] = changeValue
+    }
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+
+}
